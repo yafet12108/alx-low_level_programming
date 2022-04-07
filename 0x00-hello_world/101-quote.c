db933217bfd4c29178c129ab11ce06a415975174
yafet12108 <yafash86@gmail.com>
2022-04-07 15:09:44 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+write(1, "and that peice of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+return (1);
+}
