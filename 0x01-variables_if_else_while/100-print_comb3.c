a00bccd3942c11b66356625fc553098ca3a83aef
yafet12108 <yafash86@gmail.com>
2022-04-09 01:17:40 -0700

    comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>                                                                                                                      
+                                                                                                                                        
+/**                                                                                                                                     
+ * main - Entry point                                                                                                                   
+ *                                                                                                                                      
+ * Return: Always 0 (Success)                                                                                                           
+ */                                                                                                                                     
+int main(void)                                                                                                                          
+{                                                                                                                                       
+	int right = 48;                                                                                                                         
+	int left = 48;                                                                                                                          
+	                                                                                                                                        
+	while (left < 58)                                                                                                                       
+	{                                                                                                                                       
+		putchar(left);                                                                                                                          
+		putchar(right++);                                                                                                                       
+		if (right == 58)                                                                                                                        
+		{                                                                                                                                       
+			left++;                                                                                                                                 
+		}                                                                                                                                       
+		if (left < 58)                                                                                                                          
+		{                                                                                                                                       
+			putchar(44);                                                                                                                            
+			putchar(32);                                                                                                                            
+		}                                                                                                                                       
+		right = 48;                                                                                                                             
+	}                                                                                                                                       
+	putchar('\n');                                                                                                                          
+	return (0);                                                                                                                             
+}  
