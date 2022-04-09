b88c2c9a92c1e49f0a7f9f6c423fc22589caf06e
yafet12108 <yafash86@gmail.com>
2022-04-09 00:30:05 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int letter = 0;
+
+while (letter < 26)
+{
+putchar('a' + letter);
+letter++;
+}
+letter = 0;
+
+while (letter < 26)
+{
+putchar('A' + letter);
+putchar++;
+}
+putchar('\n');
+return(0);
+}
