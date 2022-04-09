8c698b90b09f6b576ad1a36984783d76124e2b31
yafet12108 <yafash86@gmail.com>
2022-04-09 03:41:10 -0700

    comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,9 +18,9 @@ putchar(' ');
 putchar((num2 / 10) + '0');
 putchar((num2 % 10) + '0');
 
-if (if num1 == 98 && num2 == 99)
-	continue,
-		putchar(',');
+if (num1 == 98 && num2 == 99)
+	continue;
+putchar(',');
 putchar(' ');
 }
 }
