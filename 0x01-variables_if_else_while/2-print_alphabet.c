90371072247bbdeb4cf1bd016022ac5b610e5d57
yafet12108 <yafash86@gmail.com>
2022-04-09 00:11:54 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stuio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int letter 0;
+
+while (letter < 6)
+{
+putchar('a' + letter);
+letter++;	
+}
+putchar("\n");
+return (0);
+}
