dcbb6950e9508c8d6c81afb4dacc7201666e56d3
yafet12108 <yafash86@gmail.com>
2022-04-09 00:47:43 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int current = 48;
+
+while (current < 58)
+{
+putchar(current++);
+}
+putchar('\n');
+return (0);
+}
+
